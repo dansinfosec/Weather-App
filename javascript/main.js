d2774d3b8636d4cb889b9654f39643a95118da7a
Daniel <ecomdaniel266@gmail.com>
2024-09-10 22:57:03 +0100

    Added the basic js to fetch data from the api

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+const apikey = "396e46213aa0cd618afcb2133dfa4415";
+const api_url = "http://api.openweathermap.org/data/2.5/weather?q=tetouan&units=metric";
+
+async function checkWeather() {
+    const response = await fetch(api_url + `&APPID=${apikey}`);
+    const data = await response.json();
+    
+    console.log(data);
+}
+
+checkWeather(); // Call the function to check the weather
